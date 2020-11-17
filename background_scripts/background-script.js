b44be5199b3e79337bebf3c83f950b13d560e9fb
Daniel Holzinger <d.holzinger93@gmail.com>
2020-11-17 21:57:01 +0100

    no useful commit

--- background_scripts/background-script.js
+++
@@ -1,7 +0,0 @@
-import { logMessage } from './background-module.js'
-logMessage('Hello World from Background using modules');
-
-  browser.browserAction.onClicked.addListener(() => {
-    // do not allow to open multiple time
-  browser.tabs.create({"url": "../ext_webpage/distraction_overview.html"});
-  });
--- ext_webpage/overview.js
+++
@@ -1,4 +0,0 @@
-import { logMessage } from '../background_scripts/background-module.js';
-import BrowserTabs from './components/tabs.js';
-logMessage('Hello World from extension page using modules');
-BrowserTabs.getAll();
